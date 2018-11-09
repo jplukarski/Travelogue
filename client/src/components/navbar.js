95f5c8eb81127f91844a9c236d08cdb1f5c45c04
JPlukarski <jwplukarski@gmail.com>
2018-11-08 20:43:34 -0600

    set up navbar

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,26 +1,34 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
 
+const styles = {
+    root: {
+        flexGrow: 1,
+    },
+};
 
-const Navbar = () => {
+function Navbar(props) {
+    const { classes } = props;
+
     return (
-        <div >
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton color="inherit" aria-label="Menu">
-            </IconButton>
-            <Typography variant="h6" color="inherit">
-              Travelogue
-            </Typography>
-            <Button color="inherit">Login</Button>
-          </Toolbar>
-        </AppBar>
-      </div>
+        <div className={classes.root} >
+            <AppBar position="static" style={{ color: '#2E7D32' }}>
+                <Toolbar>
+                    <Typography variant="h6" color="inherit">
+                        Travelogue
+          </Typography>
+                </Toolbar>
+            </AppBar>
+        </div>
     );
 }
 
-export default Navbar;
+Navbar.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Navbar);
