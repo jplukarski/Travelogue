648aa1dd04d5748472db6ad6561580122fa31ed2
JPlukarski <jwplukarski@gmail.com>
2018-11-21 10:56:50 -0600

    set up index in client to add custom material ui themes

--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const theme = createMuiTheme()
+
+console.log(theme)
+
+ReactDOM.render(
+    <MuiThemeProvider theme={theme}>
+        <App />
+    </MuiThemeProvider>,
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
