2dc4b5b10df5431c7311555c295d15f3f0fbda21
JPlukarski <jwplukarski@gmail.com>
2018-11-27 17:47:13 -0600

    removed grid and made table way more awesome, sortable and responsive

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,14 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Navbar from './components/navbar'
-import Trips from "./pages/trips/Trips";
-
+import Navbar from './components/navbar';
+import Trips from "./components/trips";
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Navbar />
-          <Switch>
-            <Route exact path="/" component={Trips} />
-          </Switch>
-        </div>
-      </Router>
+      <div className="App">
+        <Navbar />
+        <Trips />
+      </div>
 
     );
   }
