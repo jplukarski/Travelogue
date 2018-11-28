aa0c47105236f593d14306b50b65c05b51e33c7b
JPlukarski <jwplukarski@gmail.com>
2018-11-27 19:12:05 -0600

    started the form

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import Navbar from './components/navbar';
 import Trips from "./components/trips";
+import OutlinedTextFields from "./components/tripForm"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar />
+        <OutlinedTextFields />
         <Trips />
       </div>
 
---
+++ client/src/components/tripForm.js
@@ -0,0 +1,132 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import { withStyles } from '@material-ui/core/styles';
+import MenuItem from '@material-ui/core/MenuItem';
+import TextField from '@material-ui/core/TextField';
+
+const styles = theme => ({
+    container: {
+        display: 'flex',
+        flexWrap: 'wrap',
+    },
+    textField: {
+        marginLeft: theme.spacing.unit,
+        marginRight: theme.spacing.unit,
+    },
+    dense: {
+        marginTop: 16,
+    },
+    menu: {
+        width: 200,
+    },
+});
+
+const currencies = [
+    {
+        value: 'USD',
+        label: '$',
+    },
+    {
+        value: 'EUR',
+        label: '€',
+    },
+    {
+        value: 'BTC',
+        label: '฿',
+    },
+    {
+        value: 'JPY',
+        label: '¥',
+    },
+];
+
+class OutlinedTextFields extends React.Component {
+    state = {
+        name: 'Cat in the Hat',
+        age: '',
+        multiline: 'Controlled',
+        currency: 'EUR',
+    };
+
+    handleChange = name => event => {
+        this.setState({
+            [name]: event.target.value,
+        });
+    };
+
+    render() {
+        const { classes } = this.props;
+
+        return (
+            <form className={classes.container} noValidate autoComplete="off">
+                <TextField
+                    id="outlined-textarea"
+                    label="City"
+                    placeholder="Chicago"
+                    multiline
+                    className={classes.textField}
+                    margin="normal"
+                    variant="outlined"
+                />
+
+                <TextField
+                    id="outlined-number"
+                    label="Nights Stayed"
+                    value={this.state.age}
+                    onChange={this.handleChange('age')}
+                    type="number"
+                    className={classes.textField}
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
+                    margin="normal"
+                    variant="outlined"
+                />
+
+                <TextField
+                    id="outlined-number"
+                    label="Amount Spent"
+                    value={this.state.age}
+                    onChange={this.handleChange('age')}
+                    type="number"
+                    className={classes.textField}
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
+                    margin="normal"
+                    variant="outlined"
+                />
+
+                <TextField
+                    id="outlined-select-currency"
+                    select
+                    label="Select"
+                    className={classes.textField}
+                    value={this.state.currency}
+                    onChange={this.handleChange('currency')}
+                    SelectProps={{
+                        MenuProps: {
+                            className: classes.menu,
+                        },
+                    }}
+                    helperText="Please select your currency"
+                    margin="normal"
+                    variant="outlined"
+                >
+                    {currencies.map(option => (
+                        <MenuItem key={option.value} value={option.value}>
+                            {option.label}
+                        </MenuItem>
+                    ))}
+                </TextField>
+            </form>
+        );
+    }
+}
+
+OutlinedTextFields.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(OutlinedTextFields);
