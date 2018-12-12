2704cdfbed3ea29eecc4e00b2f22b5a51056bbcf
JPlukarski <jwplukarski@gmail.com>
2018-12-11 19:39:40 -0600

    updated total display

--- client/src/components/totalDisplay.js
+++ client/src/components/totalDisplay.js
@@ -1,85 +1,40 @@
-/* eslint-disable react/prefer-stateless-function */
-
 import React from 'react';
-import NumberFormat from 'react-number-format';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
-import API from "../utils/API";
 
 
-const styles = theme => ({
-  container: {
-    display: 'flex',
-    flexWrap: 'wrap',
+const styles = {
+  card: {
+    minWidth: 275,
   },
-  formControl: {
-    margin: theme.spacing.unit,
+  title: {
+    fontSize: 14,
   },
-});
-
-function NumberFormatCustom(props) {
-  const { inputRef, onChange, ...other } = props;
-
-  return (
-    <NumberFormat
-      {...other}
-      getInputRef={inputRef}
-      onValueChange={values => {
-        onChange({
-          target: {
-            value: values.value,
-          },
-        });
-      }}
-      thousandSeparator
-      prefix="$"
-    />
-  );
-}
-
-NumberFormatCustom.propTypes = {
-  inputRef: PropTypes.func.isRequired,
-  onChange: PropTypes.func.isRequired,
+  pos: {
+    marginBottom: 12,
+  },
 };
 
-class TotalDisplay extends React.Component {
-  state = {
+function TotalDisplay(props) {
+  const { classes } = props;
 
-    numberformat: '',
-  };
-
-  componentDidMount() {
-    this.loadTrips();
-  }
-
-  loadTrips = () => {
-    API.getTrips()
-      .then(res => console.log(res))
-      .then(res =>
-        this.setState({ numberformat: res.data[0].amountSpent }))
-      .catch(err => console.log(err))
-  }
-
-  render() {
-    const { classes } = this.props;
-    const { numberformat } = this.state;
-
-    return (
-      <div className={classes.container}>
-
-        <TextField
-          className={classes.formControl}
-          label="Total Spent"
-          value={numberformat}
-          id="formatted-numberformat-input"
-          InputProps={{
-            inputComponent: NumberFormatCustom,
-          }}
-        />
-      </div>
-    );
-  }
+  return (
+    <Card className={classes.card}>
+      <CardContent>
+        <Typography className={classes.title} color="textSecondary" gutterBottom>
+          Total Spent:
+        </Typography>
+        <Typography className={classes.pos} color="textSecondary">
+          adjective
+        </Typography>
+      </CardContent>
+      <TextField />
+    </Card>
+  );
 }
 
 TotalDisplay.propTypes = {
